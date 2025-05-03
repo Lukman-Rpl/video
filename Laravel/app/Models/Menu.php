c4aec73a2951c77b6de39801c31fe592cd2dd2ee
Lukman-Rpl <roninlukluk@gmail.com>
2025-05-03 09:06:29 +0700

    Laravel

---
+++ Laravel/app/Models/Menu.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    /** @use HasFactory<\Database\Factories\MenuFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+
+        'idkategori',
+        'menu',
+        'harga',
+        'deskripsi',
+        'gambar',
+
+    ];
+}
